0f011589eb8f1958b0fe254d20f1907022277ed2
dev-dhanushkumar <dhanush.m@kriyaetive.com>
2025-05-20 08:33:29 +0530

    update page not found

--- src/pages/not_found.rs
+++ src/pages/not_found.rs
@@ -3,8 +3,15 @@ use leptos::prelude::*;
 #[component]
 pub fn NotFound() -> impl IntoView {
     view! {
-        <div class="py-8 min-h-[calc(100vh-160px)]">
-            <p class="text-5xl text-center font-bold my-4">"404 Page Not Found"</p>
-		</div>
+        <div class="@container text-center mx-auto flex flex-col  justify-between w-full max-w-6xl px-4 sm:px-6 lg:px-8">
+            <img width={300} class="my-8 mx-auto" src="/images/404-not-found.png" alt="404 Page Not Found" />
+            <div class="p-4">
+                <h1 class="text-5xl font-bold my-4">Page Not Found</h1>
+                <p class="text-xl">
+                    "The page you're looking for is either moved or no longer exists." <br />
+                    "Please contact the site owner that linked you to the original URL and let them know their link is broken."
+                </p>
+            </div>
+        </div>
     }
 }
