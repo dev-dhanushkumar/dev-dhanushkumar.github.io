7578eb008d1fdf126e08e10a6224232b6fb71ef7
dev-dhanushkumar <dhanush.m@kriyaetive.com>
2025-05-24 20:57:36 +0530

    update crate module

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use leptos::prelude::*;
 use leptos_meta::*;
 use leptos_router::{components::*, path};
 use crate::layouts::{base_header::BaseHeader, header::Header};
-use crate::pages::{home::Home, not_found::NotFound, about::About, projects::index::Projects, posts::Posts, tags::Tags};
+use crate::pages::{home::Home, not_found::NotFound, about::About, projects::index::Projects, posts::Posts, tags::index::Tags};
 
 mod layouts;
 mod pages;
