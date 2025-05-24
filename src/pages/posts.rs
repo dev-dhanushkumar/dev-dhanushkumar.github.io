82de509483d2ed787732f8945ef7636fe52b2606
dev-dhanushkumar <dhanush.m@kriyaetive.com>
2025-05-24 20:58:35 +0530

    #122 complete Posts page completely

--- src/pages/posts.rs
+++ src/pages/posts.rs
@@ -1,11 +1,16 @@
 use leptos::prelude::*;
-
+use crate::{content::blog::load_blog_posts, components::post_by_year::PostsByYear};
 
 #[component]
 pub fn Posts() -> impl IntoView {
+    let posts = load_blog_posts().into_iter().collect::<Vec<_>>();
     view! {
-        <div class="py-8 min-h-[calc(100vh-160px)]">
-            <p class="text-5xl text-center font-bold my-4">"Posts Page"</p>
-		</div>
+        <div class="@container mx-auto flex flex-col mt-10 justify-between w-full max-w-5xl px-4 sm:px-6 lg:px-8">
+            <div class="mb-10">
+            <h1 class="text-3xl font-bold mb-2">"All Posts"</h1>
+            <p class="text-zinc-700 dark:text-zinc-300 text-lg">"Hands-on guides, code walkthroughs, and technical articles covering technologies."</p>
+            </div>
+            <PostsByYear posts={posts}/>
+        </div>
     }
 }
